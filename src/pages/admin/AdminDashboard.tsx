6bbec37af9109fc015e54d6c0b146e96409feda1
Admin <attamcraft@gmail.com>
2026-02-27 21:20:50 +0300

    fix:allow-empty-icon-in-dashboard

--- src/pages/admin/AdminDashboard.tsx
+++ src/pages/admin/AdminDashboard.tsx
@@ -1207,7 +1207,7 @@ const AdminDashboard = () => {
                     <input
                       type="text"
                       required
-                      value={currentService.icon || 'Monitor'}
+                      value={currentService.icon || ''}
                       onChange={e => setCurrentService({...currentService, icon: e.target.value})}
                       className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm border p-2"
                     />
